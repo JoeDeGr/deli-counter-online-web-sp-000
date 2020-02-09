d5eb31566b6e6789622857003e9f3b11d8c4c1a3
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-09 00:36:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is empty."
+  else
+    puts katz_deli
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Now serving #{name}. You are number #{katz_deli.length + 1}"
+end
