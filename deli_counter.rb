fae3b5384bb5808030a648de5c57c7fe9f0601d2
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-09 00:44:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     enumeratel_line = []
-    katz_deli.each_with_index {|a,i| 
+    katz_deli.each_with_index {|a,i|
     enumerated_line << "(i+1). a" }
     puts enumerated_line
   end
