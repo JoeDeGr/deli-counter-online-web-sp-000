21bb820647507f522c4299ca1e01f13c3d370f80
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-09 00:47:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     enumerated_line = []
     katz_deli.each_with_index {|a,i|
     enumerated_line << '#{i + 1}. #{a}' }
-    puts "The line is currently: #{enumerated_line}
+    puts "The line is currently: #{enumerated_line}"
   end
 end
 
