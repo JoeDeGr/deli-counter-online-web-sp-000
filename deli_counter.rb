c315349f8318188f0bec1ca0912885a88e12a128
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-09 01:11:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
-    puts "The line is now empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
   end
